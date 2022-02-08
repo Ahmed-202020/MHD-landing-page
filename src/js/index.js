f2afc475681988dbb9ba842aa4b02906ae76aea1
Ahmed-202020 <ak812554@gmail.com>
2022-02-08 16:40:02 +0200

    the last

--- src/js/index.js
+++ src/js/index.js
@@ -7,106 +7,25 @@ $(function(){
         $("html").attr("dir" , "rtl") ; 
         $("html").attr("lang" , "ar") ; 
     }) ;
-    // $(".clients").owlCarousel({
-    //     items : 10 , 
-    //     margin : 10 , 
-    //     // loop : true , 
-    //     autoplay : true , 
-    //     autoplayTimeout : 2000 , 
-    //     autoplayHoverPause : true ,
-    //     responsive: {
-    //         0:{
-    //             items:1 , 
-    //             // nav : false  , 
-    //             loop : true , 
-    //         },
-    //         600:{
-    //             items:2 , 
-    //             // nav : false , 
-    //             // loop : true , 
-    //         },
-    //         1000:{
-    //             items:3 , 
-    //             // nav : false ,
-    //             // loop : true , 
-    //         }
-            
-    //     }
-    // }) ;
-    var arrow = document.getElementsByClassName("custom-btn") ; 
-    var client = document.getElementsByClassName("client") ; 
-    var r = 0 ; 
 
-    arrow[0].onclick = () => {
-        r++ ; 
-        for(var i  of client ){
-            if(r== 0){i.style.left = "0px" ; }
-            if(r == 1){i.style.left = "-100px" ; }
-            if(r == 2){i.style.left = "-200px" ; }
-            if(r == 3){i.style.left = "-300px" ; }
-            if(r == 4){i.style.left = "-400px" ; }
-            if(r == 5){i.style.left = "-500px" ; }
-            if(r == 6){i.style.left = "-600px" ; }
-            if(r == 7){i.style.left = "-700px" ; }
-            if(r == 8){i.style.left = "-800px" ; }
-            if(r == 9){i.style.left = "-900px" ; }
-            if(r >9){r=9 ; } 
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        rtl:true,
+        margin: 15,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1 , 
+                loop: true,
+            },
+            600: {
+                items: 2 , 
+                loop: true,
+            },
+            1000: {
+                items: 6 , 
+                loop: true,
+            },
         }
-    }    
-
-    arrow[1].onclick = () => {
-        r-- ; 
-        for(var i  of client ){
-            if(r== 0){i.style.left = "0px" ; }
-            if(r == 1){i.style.left = "-100px" ; }
-            if(r == 2){i.style.left = "-200px" ; }
-            if(r == 3){i.style.left = "-300px" ; }
-            if(r == 4){i.style.left = "-400px" ; }
-            if(r == 5){i.style.left = "-500px" ; }
-            if(r == 6){i.style.left = "-600px" ; }
-            if(r == 7){i.style.left = "-700px" ; }
-            if(r == 8){i.style.left = "-800px" ; }
-            if(r == 9){i.style.left = "-900px" ; }
-            if(r < 0){r=0 ; }
-        }
-    }    
+    })
 })
-
-
-// var arrow = document.getElementsByClassName("custom-btn") ; 
-// var client = document.getElementsByClassName("client") ; 
-// var r = 0 ; 
-
-// arrow[0].onclick = () => {
-//     r++ ; 
-//     for(var i  of client ){
-//         if(r== 0){i.style.left = "0px" ; }
-//         if(r == 1){i.style.left = "-100px" ; }
-//         if(r == 2){i.style.left = "-200px" ; }
-//         if(r == 3){i.style.left = "-300px" ; }
-//         if(r == 4){i.style.left = "-400px" ; }
-//         if(r == 5){i.style.left = "-500px" ; }
-//         if(r == 6){i.style.left = "-600px" ; }
-//         if(r == 7){i.style.left = "-700px" ; }
-//         if(r == 8){i.style.left = "-800px" ; }
-//         if(r == 9){i.style.left = "-900px" ; }
-//         if(r >9){r=9 ; } 
-//     }
-// }    
-
-// arrow[1].onclick = () => {
-//     r-- ; 
-//     for(var i  of client ){
-//         if(r== 0){i.style.left = "0px" ; }
-//         if(r == 1){i.style.left = "-100px" ; }
-//         if(r == 2){i.style.left = "-200px" ; }
-//         if(r == 3){i.style.left = "-300px" ; }
-//         if(r == 4){i.style.left = "-400px" ; }
-//         if(r == 5){i.style.left = "-500px" ; }
-//         if(r == 6){i.style.left = "-600px" ; }
-//         if(r == 7){i.style.left = "-700px" ; }
-//         if(r == 8){i.style.left = "-800px" ; }
-//         if(r == 9){i.style.left = "-900px" ; }
-//         if(r < 0){r=0 ; }
-//     }
-// }    
