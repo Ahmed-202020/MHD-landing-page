81d9e4421e861b363485d1e9371ceacad660c251
Ahmed-202020 <ak812554@gmail.com>
2022-02-16 12:06:05 +0200

    the last

--- src/js/index.js
+++ src/js/index.js
@@ -9,10 +9,14 @@ $(function(){
     }) ;
 
     $('.owl-carousel').owlCarousel({
-        loop: false,
+        // loop: true,
         rtl:true,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        autoHeight : true,
         margin: 15,
         nav: true,
+        
         responsive: {
             0: {
                 items: 1 , 
